1a4ba63c59690a8514aa07931e646f0a99d77cd6
Sonu P <sonup@Sonus-MacBook-Pro.local>
2021-10-31 15:55:38 +0530

    update env variables

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+const name=process.env.NAME;
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log(`${name}`);
     await sleep(5000);
   }
 }
